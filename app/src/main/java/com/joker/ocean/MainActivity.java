1ff673c58807bb63cc5539ff4eec19ba5bd2d853
duominike <jimmy882010@163.com>
2017-03-26 16:29:50 +0800

    commit tv files

--- app/src/main/java/com/joker/ocean/MainActivity.java
+++ app/src/main/java/com/joker/ocean/MainActivity.java
@@ -6,9 +6,11 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.joker.ocean.base.BaseActivity;
+<<<<<<< 9c5943ecc200aa900d49f319cb54e8450b2bd3c0
 import com.joker.ocean.testview.TestJsbridgeActivity;
 import com.joker.ocean.testview.TestVerticalSeekbarActiivty;
 import com.joker.ocean.testview.TextCommentListActivity;
+import com.joker.ocean.tvdemo.TVDemoActivity;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener{
     private TextView m_tvTestView;
@@ -33,7 +35,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
         Intent intent;
         switch (view.getId()){
             case R.id.tv_customview:
-                intent = new Intent(MainActivity.this, TextCommentListActivity.class);
+                intent = new Intent(MainActivity.this, TVDemoActivity.class);
                 startActivity(intent);
                 break;
         }
---
+++ app/src/main/java/com/joker/ocean/tvdemo/model/DemoModle.java
@@ -0,0 +1,112 @@
+package com.joker.ocean.tvdemo.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by joker on 17-3-26.
+ */
+
+@SuppressWarnings("serial")
+public class DemoModle implements Serializable {
+
+    private int image;
+
+    private String name;
+
+    private String url;
+
+    private String info;
+
+    private String date;
+
+    private List childs;
+
+
+    private int postion;
+
+
+
+    public DemoModle() {
+        super();
+    }
+
+    public DemoModle(int image, String name) {
+        super();
+        this.image = image;
+        this.name = name;
+    }
+    public DemoModle(int image, String name,String url) {
+        super();
+        this.image = image;
+        this.name = name;
+        this.url = url;
+    }
+
+    public DemoModle(int image, String name, String url, int postion) {
+        super();
+        this.image = image;
+        this.name = name;
+        this.url = url;
+        this.postion = postion;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public List getChilds() {
+        return childs;
+    }
+
+    public void setChilds(List childs) {
+        this.childs = childs;
+    }
+
+    public int getPostion() {
+        return postion;
+    }
+
+    public void setPostion(int postion) {
+        this.postion = postion;
+    }
+
+
+
+}
