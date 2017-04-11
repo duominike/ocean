609bc2770051e4d1071afa9cd345aa5435e2ad94
liwenle <liwenle@mail.51vv.com>
2017-04-11 21:43:53 +0800

    commit tv files

--- app/src/main/java/com/joker/ocean/tvdemo/view/FocusAdapter.java
+++ app/src/main/java/com/joker/ocean/tvdemo/view/FocusAdapter.java
@@ -6,10 +6,14 @@ package com.joker.ocean.tvdemo.view;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.joker.ocean.R;
 import com.joker.ocean.tvdemo.model.DemoModle;
 
 import java.util.List;
@@ -48,14 +52,14 @@ public class FocusAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-		/*LayoutInflater mInflater = LayoutInflater.from(mContext);
+		LayoutInflater mInflater = LayoutInflater.from(mContext);
 		View view  = mInflater.inflate(
 				R.layout.layout_focus_item,parent);
 		ImageView mImageView = (ImageView) view.findViewById(R.id.photo);
 		TextView mTextView = (TextView) view.findViewById(R.id.title);
 		mImageView.setBackgroundResource(mLists.get(position).getImage());
-		mTextView.setText(mLists.get(position).getName());*/
-        return null;
+		mTextView.setText(mLists.get(position).getName());
+        return view;
     }
 
 }
