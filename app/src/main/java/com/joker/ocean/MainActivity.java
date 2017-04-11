0b228df180d1ca4566419a06446a01e095c5da63
liwenle <liwenle@mail.51vv.com>
2017-04-11 21:43:53 +0800

    commit tv files

--- app/src/main/java/com/joker/ocean/MainActivity.java
+++ app/src/main/java/com/joker/ocean/MainActivity.java
@@ -2,6 +2,8 @@ package com.joker.ocean;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,6 +16,7 @@ import com.joker.ocean.tvdemo.TVDemoActivity;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener{
     private TextView m_tvTestView;
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +31,16 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
 
     private void setUp(){
         m_tvTestView.setOnClickListener(this);
+        m_tvTestView.setFocusable(false);
+        m_tvTestView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    Log.i(TAG, "has Focus");
+                    m_tvTestView.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                }
+            }
+        });
     }
 
     @Override
@@ -39,5 +52,27 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
                 startActivity(intent);
                 break;
         }
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        Log.i(TAG, "dispatchKeyEvent: ----->> " + event.getKeyCode());
+        if(event.getKeyCode() == KeyEvent.KEYCODE_ENTER){
+            if(m_tvTestView.hasFocus()){
+                startActivity(new Intent(this, TVDemoActivity.class));
+            }
+        }
+        return super.dispatchKeyEvent(event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_DPAD_DOWN){
+            Log.i(TAG, "onKeyUp: ----->> ");
+            m_tvTestView.setFocusable(true);
+            m_tvTestView.requestFocus();
+            return true;
+        }
+        return super.onKeyUp(keyCode, event);
     }
 }
